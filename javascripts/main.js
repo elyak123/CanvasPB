f2b2d90c6e8c271f12d92533ff5507b17f38504b
elyak123 <elyak.123@gmail.com>
2014-10-08 09:15:28 -0500

    Revert "ya termine el circulo, grabate porfavor"
    This reverts commit 9810efbd5424c391a10cdde9547d19ff07125732.
    
    Revert "Revert "bla bla""
    
    This reverts commit
    4f8fa7d6abc861f83bb21571c039ed0145230517.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,42 +1,37 @@
+var dibujo;
+var lienzo;
+
+
 function inicio(){
-	var dibujo;
-	var lienzo;
 	dibujo = document.getElementById("dibujito");
 	lienzo = dibujo.getContext("2d");	
-	//lienzo.moveTo(1,1);	
-	//lienzo.lineTo(1,299);
-	//lienzo.lineTo(299,299);
-	//lienzo.lineTo(299,1);
-	//lienzo.lineTo(1,1);
-	//lienzo.strokeStyle = "#F00";
-	//lienzo.stroke();
+	lienzo.moveTo(1,1);	
+	lienzo.lineTo(1,299);
+	lienzo.lineTo(299,299);
+	lienzo.lineTo(299,1);
+	lienzo.lineTo(1,1);
+	lienzo.strokeStyle = "#F00";
+	lienzo.stroke();
 	//lienzo.beginPath(); permite crear otro trazo con un color diferente
-	 lienzo.beginPath();
-	 lienzo.strokeStyle = "#F00";
-	 //lienzo.arc(x del centro, y del centro, radio del ciculo, circunferencia en radianes)
-	 lienzo.arc(150,150, 100, (Math.PI * 2 ), false);
-	 lienzo.closePath();
-	 lienzo.stroke();
-	 dibujarGrilla(lienzo);
+	// lienzo.beginPath();
+	// lienzo.arc(x del centro, y del centro, radio del ciculo, circunferencia en radianes)
+	// lienzo.arc(150,150, 100, (Math.PI * 2 ), false);
+	// lienzo.closePath();
+	// lienzo.stroke();
+	 dibujarGrilla();
 
 }
-function dibujarGrilla (l){
+function dibujarGrilla (){
 	var ancho = 300, alto = 300;
-	var nlineas = 30;
-	var anchoColumna = ancho / nlineas;
-	var anchoFila = alto / nlineas;
-		for(lapiz = 0; lapiz <= 300; lapiz = lapiz + anchoColumna )
+		for(linea = 0; linea < 300; linea = linea + 10 )
 	{
 		//lienzo.moveTo( linea, 0 );
 		//lienzo.lineTo( linea, alto);
-		l.beginPath();
-		l.strokeStyle = "#555555"
-		l.moveTo( lapiz, 0);
-		l.lineTo( lapiz, ancho);
-		l.stroke();
-		l.moveTo( 0, lapiz);
-		l.lineTo( ancho, lapiz);
-		l.stroke();
+		lienzo.moveTo( 0, linea);
+		lienzo.lineTo( linea, ancho);
+		lienzo.stroke();
+		lienzo.moveTo( linea, 0);
+		lienzo.lineTo( alto, linea);
 	}
 }
 
