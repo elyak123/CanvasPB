578b2c62b93ca2942ae79cbeaab0065eeed11d49
elyak123 <elyak.123@gmail.com>
2014-10-10 22:52:41 -0500

    Para grilla en x pero hay errores en el ciclo

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,37 +3,33 @@ function inicio(){
 	var lienzo;
 	dibujo = document.getElementById("dibujito");
 	lienzo = dibujo.getContext("2d");	
-	lienzo.beginPath();
-	lienzo.strokeStyle = "#F00";
+	//lienzo.beginPath(); permite crear otro trazo con un color diferente
+	 lienzo.beginPath();
+	 lienzo.strokeStyle = "#F00";
 	 //lienzo.arc(x del centro, y del centro, radio del ciculo, circunferencia en radianes)
-	circulo(lienzo)
-	dibujarGrilla(lienzo);
-	areabcurva(lienzo);
-
-}
-function circulo (c){
-	c.arc(150,150, 100, (Math.PI * 2 ), false);
-	c.closePath();
-	c.stroke();
-	c.fillStyle ="red";
-	c.fill()
+	 lienzo.arc(150,150, 100, (Math.PI * 2 ), false);
+	 lienzo.closePath();
+	 lienzo.stroke();
+	 dibujarGrilla(lienzo);
 }
 function dibujarGrilla (l){
+	var x;
+	var y;
 	var ancho = 300, alto = 300;
 	var nlineas = 30;
 	var anchoColumna = ancho / nlineas;
 	var anchoFila = alto / nlineas;
-		for(lapiz = 0; lapiz <= 300; lapiz = lapiz + anchoColumna )
+		for(lapiz = 300; 0 => lapiz <= 300; lapiz = lapiz - anchoColumna )
 	{
-		//lienzo.moveTo( linea, 0 );
-		//lienzo.lineTo( linea, alto);
 		l.beginPath();
 		l.strokeStyle = "#555555"
-		l.moveTo( lapiz, 0);
-		l.lineTo( lapiz, ancho);
-		l.stroke();
 		l.moveTo( 0, lapiz);
+		l.lineTo( ancho - lapiz, 0);
+		l.stroke();
+		l.moveTo( lapiz, alto);
 		l.lineTo( ancho, lapiz);
 		l.stroke();
 	}
 }
+
+
