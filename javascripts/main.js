12715d1a360e02f3ef16b21d39018153f8958c00
elyak123 <elyak.123@gmail.com>
2014-10-11 14:55:27 -0500

    Tarea de grilla diagonal sin circulo relleno

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,22 +13,33 @@ function inicio(){
 	 dibujarGrilla(lienzo);
 }
 function dibujarGrilla (l){
-	var x;
-	var y;
 	var ancho = 300, alto = 300;
 	var nlineas = 30;
 	var anchoColumna = ancho / nlineas;
 	var anchoFila = alto / nlineas;
-		for(lapiz = 300; 0 => lapiz <= 300; lapiz = lapiz - anchoColumna )
+	for(lapiz = 0; lapiz <= 300; lapiz = lapiz + anchoColumna )
 	{
 		l.beginPath();
 		l.strokeStyle = "#555555"
 		l.moveTo( 0, lapiz);
-		l.lineTo( ancho - lapiz, 0);
+		l.lineTo( ancho, alto + lapiz);
 		l.stroke();
-		l.moveTo( lapiz, alto);
+		l.closePath();
+		l.beginPath();
+		l.moveTo( 0, alto - lapiz );
+ 		l.lineTo( ancho - lapiz, 0 );
+		l.stroke();
+		l.closePath();
+		l.beginPath();
+		l.moveTo( ancho - lapiz, 0 );
 		l.lineTo( ancho, lapiz);
 		l.stroke();
+		l.closePath();
+		l.beginPath();
+		l.moveTo( ancho, lapiz );
+		l.lineTo( lapiz, alto );
+		l.stroke();
+		l.closePath();
 	}
 }
 
