a1c3d0b347a18b2c040f87a6d81c31ac6e63d846
elyak123 <elyak.123@gmail.com>
2014-10-12 11:24:22 -0500

    tarea de circulo con grilla en diagonal, falta el evento.

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,14 +4,33 @@ function inicio(){
 	dibujo = document.getElementById("dibujito");
 	lienzo = dibujo.getContext("2d");	
 	//lienzo.beginPath(); permite crear otro trazo con un color diferente
-	 lienzo.beginPath();
-	 lienzo.strokeStyle = "#F00";
-	 //lienzo.arc(x del centro, y del centro, radio del ciculo, circunferencia en radianes)
-	 lienzo.arc(150,150, 100, (Math.PI * 2 ), false);
-	 lienzo.closePath();
-	 lienzo.stroke();
-	 dibujarGrilla(lienzo);
+	lienzo.beginPath();
+	//lienzo.strokeStyle = "#F00";
+	lienzo.moveTo(0,0);
+	lienzo.lineTo(300, 0);
+	lienzo.stroke();
+	lienzo.lineTo(300,300);
+	lienzo.stroke();
+	lienzo.lineTo(0, 300 );
+	lienzo.stroke();
+	lienzo.lineTo(0,0);
+	lienzo.stroke();
+	lienzo.closePath();
+	dibujarGrilla(lienzo);
+	circulo(lienzo);
 }
+function circulo (c){
+	//lienzo.arc(x del centro, y del centro, radio del ciculo, circunferencia en radianes)
+	c.beginPath();
+	c.fillStyle ="#F54D27";
+	c.arc(150,150, 100, (Math.PI * 2 ), false);
+	c.stroke();
+	c.closePath();
+	c.fillStyle ="#F54D27";
+	c.fill();
+	c.closePath();
+}
+
 function dibujarGrilla (l){
 	var ancho = 300, alto = 300;
 	var nlineas = 30;
